dca621fc9a118cc914e8809726cdbc89235d11b0
WillSchwieder <wschwied@chicagobooth.edu>
2022-04-01 16:15:39 +0000

    215 complete, done with dice project

--- about_classes.rb
+++ about_classes.rb
@@ -183,8 +183,8 @@ class AboutClasses < Neo::Koan
     assert_equal "[1, 2, 3]", array.to_s
     assert_equal "[1, 2, 3]", array.inspect
 
-    assert_equal "\"STRING\"", "STRING".to_s
-    assert_equal "STRING", "STRING".inspect
+    assert_equal "STRING", "STRING".to_s
+    assert_equal "\"STRING\"", "STRING".inspect
   end
 
 end
--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,22 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  attr_reader :values
+  def roll(num_rolls)
+    @values = []
+    num_rolls.times do
+      num = rand(1...6)
+      @values << num
+    end
+  end
+    
+end
+
+# dice = DiceSet.new
+
+# print(dice.roll(6))
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
